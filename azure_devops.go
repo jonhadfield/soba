b1898d7c1a33f3137ef5a99379fd91e2aaf79969
Jon Hadfield <jon@lessknown.co.uk>
2024-03-10 20:56:21 +0000

    add support for Azure DevOps repository backups.

---
+++ azure_devops.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/jonhadfield/githosts-utils"
+	"gitlab.com/tozd/go/errors"
+)
+
+func AzureDevOps(backupDir string) *ProviderBackupResults {
+	logger.Println("backing up Azure DevOps repos")
+
+	azureDevOpsHost, err := githosts.NewAzureDevOpsHost(githosts.NewAzureDevOpsHostInput{
+		Caller:           appName,
+		BackupDir:        backupDir,
+		DiffRemoteMethod: os.Getenv(envAzureDevOpsCompare),
+		UserName:         os.Getenv(envAzureDevOpsUserName),
+		PAT:              os.Getenv(envAzureDevOpsPAT),
+		Orgs:             getOrgsListFromEnvVar(envAzureDevOpsOrgs),
+		BackupsToRetain:  getBackupsToRetain(envAzureDevOpsBackups),
+		LogLevel:         getLogLevel(),
+	})
+	if err != nil {
+		return &ProviderBackupResults{
+			Provider: providerNameAzureDevOps,
+			Results: githosts.ProviderBackupResult{
+				BackupResults: []githosts.RepoBackupResults{},
+				Error:         errors.Wrap(err, "failed to create AzureDevOps host"),
+			},
+		}
+	}
+
+	return &ProviderBackupResults{
+		Provider: providerNameAzureDevOps,
+		Results:  azureDevOpsHost.Backup(),
+	}
+}
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ toolchain go1.21.0
 require (
 	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
 	github.com/hashicorp/go-retryablehttp v0.7.5
-	github.com/jonhadfield/githosts-utils v0.0.0-20240227215907-fdbfc9a27143
+	github.com/jonhadfield/githosts-utils v0.0.0-20240310201523-4a29947afd0b
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.9.0
 	gitlab.com/tozd/go/errors v0.8.1
@@ -16,9 +16,11 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/microsoft/azure-devops-go-api/azuredevops/v7 v7.1.0 // indirect
 	github.com/peterhellberg/link v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
